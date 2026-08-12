c18552d7f023266175f0940f8be940863bc5afae
joshie-wu <josh.wenjun@gmail.com>
2026-08-11 19:45:46 -0500

    Convering to a .mdx file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   site: 'https://joshie-wu.gitlab.io',
   base: '/i-quant-data',
   outDir: 'public',
   publicDir: 'static',
+  integrations: [mdx()],
 });
